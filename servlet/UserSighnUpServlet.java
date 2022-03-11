aba6626ca5bd65a70c8abb9a2720a3ef1557bddc
github publicsaito3110 <publicbusinesssito3110@gmail.com>
2022-03-11 18:24:44 +0900

    処理の最適化
    処理が複雑だったため

--- servlet/UserSighnUpServlet.java
+++ servlet/UserSighnUpServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.UserInfoBean;
+import bl.UserInfoBl;
 import common.CommonUtil;
 
 /**
@@ -38,8 +42,11 @@ public class UserSighnUpServlet extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 
-		//新規登録の結果を返すための変数
-		boolean sighnUpResult = false;
+
+
+		boolean sighnUpResult = false;       //新規登録の結果を返すための変数
+		boolean afterForm = true;                   //ポップアップを表示させるための変数
+
 
 		//sighn-up.jsp からの値を受け取る
 		String id = request.getParameter("id");
@@ -48,55 +55,57 @@ public class UserSighnUpServlet extends HttpServlet {
 		String gender = request.getParameter("gender");
 
 
-		//------------バリデーションチェック------------------------------------------------
-		boolean vali1 = false;
-		boolean vali2 = false;
-		boolean vali3 = false;
-		boolean vali4 = false;
+		//バリデーションチェック
+		boolean vali1 = CommonUtil.validIdStatus(id);
+		boolean vali2 = CommonUtil.validNameStatus(name);
+		boolean vali3 = CommonUtil.validNameKanaStatus(nameKana);
+		boolean vali4 = CommonUtil.validGenderStatus(gender);
 
-		if(CommonUtil.validIdStatus(id)) {                            //idの判定
-			vali1 = true;
-		}else if(CommonUtil.validNameStatus(name)) {                 //nameの判定
-			vali2 = true;
-		}else if(CommonUtil.validNameKanaStatus(nameKana)) {        //nameKanaの判定
-			vali3 = true;
-		}else if(CommonUtil.validGenderStatus(gender)) {            //genderの判定
-			vali4 = true;
-		}
-
-
+		//開発用(valiの値を確認)------------------
 		System.out.println(vali1);
 		System.out.println(vali2);
 		System.out.println(vali3);
 		System.out.println(vali4);
+//---------------------------------------------
+
+		if(vali1 && vali2 && vali3 && vali4) {         //バリデーションチェックがセーフのとき
+
+			//ArrayListに値を詰めてBLに引き渡す
+			List<UserInfoBean> userInfoList = new ArrayList<>();
+			UserInfoBean bean = new UserInfoBean();
+
+			bean.setId(id);
+			bean.setName(name);
+			bean.setNameKana(nameKana);
+			bean.setGender(gender);
+
+			userInfoList.add(bean);
+
+			UserInfoBl bl = new UserInfoBl();
+			//ユーザの新規登録結果を取得
+			sighnUpResult = bl.userSighnUp(userInfoList);
+
+		}else{                                             //バリデーションチェックが1つでもアウトのときvaliを渡す
+			//valiを渡す
+			request.setAttribute("vali1", vali1);
+			request.setAttribute("vali2", vali2);
+			request.setAttribute("vali3", vali3);
+			request.setAttribute("vali4", vali4);
+
+			//入力された値を返す(gender以外)
+			request.setAttribute("id", id);
+			request.setAttribute("name", name);
+			request.setAttribute("nameKana", nameKana);
+
+			boolean valiEroor = true;                    //バリデーションが1つ以上アウトだった結果を引き渡す
+			request.setAttribute("valiEroor", valiEroor);
+		}
 
 
-//		if(vali1 && vali2 && vali3 && vali4) {         //バリデーションチェックがセーフのとき
-//
-//			//ArrayListに値を詰めてBLに引き渡す
-//			List<UserInfoBean> userInfoList = new ArrayList<>();
-//			UserInfoBean bean = new UserInfoBean();
-//
-//			bean.setId(id);
-//			bean.setName(nameKana);
-//			bean.setNameKana(nameKana);
-//			bean.setGender(gender);
-//
-//			userInfoList.add(bean);
-//
-//			UserInfoBl bl = new UserInfoBl();
-//			sighnUpResult = bl.userSighnUp(userInfoList);
-//
-//		}else {         //バリデーションチェックに1つでもアウトのとき
-//			request.setAttribute("vali1", vali1);
-//			request.setAttribute("vali2", vali2);
-//			request.setAttribute("vali3", vali3);
-//			request.setAttribute("vali4", vali4);
-//		}
-//
-//		//sighn-up.jspに結果を返す
-//		request.setAttribute("sighnUpResult", sighnUpResult);
-//		request.getRequestDispatcher("/WEB-INF/jsp/sighn-up.jsp").forward(request, response);
+		//sighn-up.jspに結果を返す
+		request.setAttribute("sighnUpResult", sighnUpResult);
+		request.setAttribute("afterForm", afterForm);
+		request.getRequestDispatcher("/WEB-INF/jsp/sighn-up.jsp").forward(request, response);
 	}
 
 }
