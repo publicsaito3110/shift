f784001895d9547594bfa586ba21504069b7f7fb
github publicsaito3110 <publicbusinesssito3110@gmail.com>
2022-03-18 12:26:46 +0900

    DB登録情報追加に伴う処理の追加

--- common/Const.java
+++ common/Const.java
@@ -14,9 +14,15 @@ public class Const {
 	public static final String valiAlpha = "a-zA-Z"; //半角英字
 	public static final String valiGana = "ぁ-ん";   //全角ひらがな
 	public static final String valiKana = "ァ-ヶー";  //全角カタカナ
-	public static final String valiKanji = "\\\\u4E00-\\\\u9FFF\\\\u3005-\\\\u3007";  //全角漢字
-	public static final String valiAllowKigou = "ー　 ";  //長音, 半角全角スペース
+	public static final String valiKanji = "亜-黑";  //全角漢字
 	public static final String valiGender = "1|2";  //1または2
+	public static final String valiAllowKigou = "ー　 ";  //長音, 半角全角スペース
+	public static final String valiAllowKigouVer = "-";  //長音, 半角全角スペース
+	public static final String valiEmail = "^(([0-9a-zA-Z!#$%&'*+-/=?^_`{}|~]+(.[0-9a-zA-Z!#$%&'*+-/=?^_`{}|~]+)*)|(\"[^\"]*\"))@[0-9a-zA-Z!#$%&'*+-/=?^_`{}|~]+(.[0-9a-zA-Z!#$%&'*+-/=?^_`{}|~]+)*$";  //メールの正規表現
+	public static final String valiDelFlag = "1";  //1
+
+	public static final String valiXSSScript = "<>&\"'";
+	public static final String valiXSSUrl = "http://|https://";
 
 
 	//------------------
@@ -26,5 +32,10 @@ public class Const {
 	public static final String erName = "半角英字または全角日本語(20文字以内)で入力してください";
 	public static final String erNameKana = "全角カタカナ(40文字以内)で入力してください";
 	public static final String erGender = "不正な値です";
+	public static final String erPassword = "半角英数字(8文字未満)で入力してください";
+	public static final String erAddress = "使用できない文字が含まれています";
+	public static final String erTel = "半角数字(15文字以内)で入力してください";
+	public static final String erEmail = "使用できない文字が含まれています";
+	public static final String erNote = "400文字以内で入力してください";
 
 }
