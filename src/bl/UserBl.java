df31188fb87750046fafe390d9324441e79b7e3e
github publicsaito3110 <publicbusinesssito3110@gmail.com>
2022-04-08 13:41:30 +0900

    offsetの処理を追加

--- src/bl/UserBl.java
+++ src/bl/UserBl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bean.UserBean;
+import common.Const;
 import dao.UserDao;
 
 
@@ -36,23 +37,15 @@ public class UserBl {
 			return userList;
 	}
 
-	/**
-	 * 登録しているユーザ全員を取得する
-	 */
-	public List<UserBean> selectUserAll(int offset){
-
-		//DAOの戻り値をuserListで受け取る
-		List<UserBean> userList = new ArrayList<>();
-		UserDao dao = new UserDao();
-		userList = dao.selectUserAll(offset);
-
-		return userList;
-	}
 
 	/**
 	 * キーワードに該当するユーザ全員を表示するメソッド
 	 */
-	public List<UserBean> selectUserByKeyWord(int offset, String keyWord){
+	public List<UserBean> selectUserByKeyWord(String page, String keyWord){
+
+
+		//受け取ったページに対応したoffsetを取得する
+		int offset = this.toIntReturnOffsetByPage(page);
 
 		//DAOの戻り値をuserListで受け取る
 		List<UserBean> userList = new ArrayList<>();
@@ -101,4 +94,33 @@ public class UserBl {
 		return isUpdateResult;
 	}
 
+
+
+	/**
+	 *指定したページに対応したoffsetを返す
+	 *@param String page, got page by user-list.jsp
+	 *@return int, calced offset
+	 */
+	public int toIntReturnOffsetByPage(String page) {
+
+		//1回のSQLで表示する件数
+		int pageLimitPer = Integer.parseInt(Const.PAGE_LIMIT);
+
+
+		//ユーザ一覧に表示するためページに対応した(データn件目～)に初期値0を代入する
+		int offset = Const.OFFSET_FIRST;
+
+
+		//ページ数を指定されたとき
+		if (page != null) {
+
+			//ユーザ一覧するため指定したページに対応した(データn件目～)をoffsetに代入する
+			int nowPage = Integer.parseInt(page);
+			nowPage = (nowPage - 1) * pageLimitPer;
+
+			offset = nowPage;
+		}
+
+		return offset;
+	}
 }
