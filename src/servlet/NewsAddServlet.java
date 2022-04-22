b4493416665a7278587b36a48831d75657f11817
github publicsaito3110 <publicbusinesssito3110@gmail.com>
2022-04-22 20:05:30 +0900

    お知らせの新規登録をAjax化に仕様変更

--- src/servlet/NewsAddServlet.java
+++ src/servlet/NewsAddServlet.java
@@ -18,7 +18,7 @@ import common.Const;
  * Servlet implementation class NewsAddServlet
  */
 @WebServlet("/NewsAddServlet")
-public class NewsAddServlet extends BaseServlet {
+public class NewsAddServlet extends BaseAdministratorServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
@@ -32,118 +32,105 @@ public class NewsAddServlet extends BaseServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+	protected void isAdministorator(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
-		//現在の日付を取得
-		LocalDate now = LocalDate.now();
+    	//news-add.jspから値を受け取る
+    	String date = request.getParameter("date");
+    	String title = request.getParameter("title");
+    	String content = request.getParameter("content");
 
-		//現在の日付(YYYY-MM-DD)を取得する
-		String nowDate = now.toString();
+    	//現在の日付を取得
+    	LocalDate now = LocalDate.now();
 
-		//現在の日付から上限(3ヵ月後)の日付を取得
-		LocalDate limitMonth= now.plusMonths(3);
-
-		//上限の日付(YYYY-MM-DD)を取得する
-		String maxDate = limitMonth.toString();
-
-
-		//Postで受け取ったとき
-		if (Const.DO_POST.equals(request.getMethod())) {
-
-			//news-add.jspから値を受け取る
-			String date = request.getParameter("date");
-			String title = request.getParameter("title");
-			String content = request.getParameter("content");
+    	//現在の日付から上限(3ヵ月後)の日付を取得
+    	LocalDate limitMonth= now.plusMonths(Const.NEWS_ADD_MAX_MONTH);
 
 
-			//------------------------
-			//バリデーションチェック
-			//------------------------
+    	//------------------------
+    	//バリデーションチェック
+    	//------------------------
 
-			//いずれかの入力値が未入力のとき
-			if (date.isEmpty() || title.isEmpty() || content.isEmpty()) {
+    	//いずれかの入力値が未入力のとき
+    	if (date.isEmpty() || title.isEmpty() || content.isEmpty()) {
 
-				//引き渡す値を設定
-				request.setAttribute("nowDate", nowDate);
-				request.setAttribute("maxDate", maxDate);
+    		//引き渡す値を設定
+    		request.setAttribute("afterFormFlag", true);
+    		request.setAttribute("popTitle", "新規登録結果");
+    		request.setAttribute("resultText", "[エラー] 未入力の値があります");
+    		request.setAttribute("result", false);
 
-				//画面遷移
-				request.getRequestDispatcher("/WEB-INF/jsp/news-add.jsp").forward(request, response);
-				return;
-			}
+    		//画面遷移
+    		request.getRequestDispatcher("/WEB-INF/jsp/common/pop-window.jsp").forward(request, response);
+    		return;
+    	}
 
-			//入力された日付をLocalDateで取得
-			LocalDate localDate = LocalDate.parse(date);
+    	//入力された日付をLocalDateで取得
+    	LocalDate localDate = LocalDate.parse(date);
 
-			//入力された日付が上限の日付より後または現在より前の日付だったとき
-			if (localDate.isAfter(limitMonth) || localDate.isBefore(now)) {
+    	//入力された日付が上限の日付より後または現在より前の日付だったとき
+    	if (localDate.isAfter(limitMonth) || localDate.isBefore(now)) {
 
-				//引き渡す値を設定
-				request.setAttribute("nowDate", nowDate);
-				request.setAttribute("maxDate", maxDate);
+    		//引き渡す値を設定
+    		request.setAttribute("afterFormFlag", true);
+    		request.setAttribute("popTitle", "新規登録結果");
+    		request.setAttribute("resultText", "[エラー] 不正な値を検知しました");
+    		request.setAttribute("result", false);
 
-				//画面遷移
-				request.getRequestDispatcher("/WEB-INF/jsp/news-add.jsp").forward(request, response);
-				return;
-			}
+    		//画面遷移
+    		request.getRequestDispatcher("/WEB-INF/jsp/common/pop-window.jsp").forward(request, response);
+    		return;
+    	}
 
-			//dateをymd(YYYYMMDD)に変換
-			String ymd = date.replaceAll("/", "");
+    	//dateをymd(YYYYMMDD)に変換
+    	String ymd = date.replaceAll("/", "");
 
-			//入力値をエスケープ
-			title = CommonUtil.replaceEscapeChar(title);
-			content = CommonUtil.replaceEscapeChar(content);
+    	//入力値をエスケープ
+    	title = CommonUtil.replaceEscapeChar(title);
+    	content = CommonUtil.replaceEscapeChar(content);
 
-			//-----------
-			//SQLの実行
-			//-----------
+    	//-----------
+    	//SQLの実行
+    	//-----------
 
-			//newsBeanに受け取った値を格納する
-			NewsBean newsBean = new NewsBean();
+    	//newsBeanに受け取った値を格納する
+    	NewsBean newsBean = new NewsBean();
 
-			newsBean.setYmd(ymd);
-			newsBean.setContent(title);
-			newsBean.setContent(content);
+    	newsBean.setYmd(ymd);
+    	newsBean.setContent(title);
+    	newsBean.setContent(content);
 
-			NewsBl bl = new NewsBl();
+    	NewsBl bl = new NewsBl();
 
-			boolean isResult = bl.insertNews(newsBean);
+    	boolean isResult = bl.insertNews(newsBean);
 
 
-			//-----------
-			//SQLの判定
-			//-----------
+    	//-----------
+    	//SQLの判定
+    	//-----------
 
-			//SQLが失敗したとき
-			if (!isResult) {
+    	//SQLが失敗したとき
+    	if (!isResult) {
 
-				//引き渡す値を設定
-				request.setAttribute("nowDate", nowDate);
-				request.setAttribute("maxDate", maxDate);
+    		//引き渡す値を設定
+    		request.setAttribute("afterFormFlag", true);
+    		request.setAttribute("popTitle", "新規登録結果");
+    		request.setAttribute("resultText", "[エラー] 新規登録に失敗しました");
+    		request.setAttribute("result", false);
 
-				//画面遷移
-				request.getRequestDispatcher("/WEB-INF/jsp/news-add.jsp").forward(request, response);
-				return;
-			}
+    		//画面遷移
+    		request.getRequestDispatcher("/WEB-INF/jsp/common/pop-window.jsp").forward(request, response);
+    		return;
+    	}
 
-			//引き渡す値を設定
-			request.setAttribute("nowDate", nowDate);
-			request.setAttribute("maxDate", maxDate);
+    	//引き渡す値を設定
+    	request.setAttribute("afterFormFlag", true);
+    	request.setAttribute("popTitle", "新規登録結果");
+    	request.setAttribute("resultText", "[エラー] 新規登録に失敗しました");
+    	request.setAttribute("result", true);
 
-			//画面遷移
-			request.getRequestDispatcher("/WEB-INF/jsp/news-add.jsp").forward(request, response);
-		}
-
-		//Getで受け取ったとき
-		if (Const.DO_GET.equals(request.getMethod())) {
-
-			//引き渡す値を設定
-			request.setAttribute("nowDate", nowDate);
-			request.setAttribute("maxDate", maxDate);
-
-			//画面遷移
-			request.getRequestDispatcher("/WEB-INF/jsp/news-add.jsp").forward(request, response);
-		}
-	}
+    	//画面遷移
+    	request.getRequestDispatcher("/WEB-INF/jsp/common/pop-window.jsp").forward(request, response);
+    }
 }
